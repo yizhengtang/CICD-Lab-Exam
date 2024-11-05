586a94671b689ce82b81a2ca8dbb843673b61e39
Yi Zheng Tang <yzhengtang4@gmail.com>
2024-11-05 10:04:56 +0000

    Created a project called Lab Exam

---
+++ src/test/java/ie/atu/labexam/LabExamApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
