4e9df7c154aa9543c1ef35345abfd942b8563603
Yi Zheng Tang <yzhengtang4@gmail.com>
2024-11-05 11:42:33 +0000

    Validation

--- src/main/java/ie/atu/labexam/ControllerAdvice.java
+++ src/main/java/ie/atu/labexam/ControllerAdvice.java
@@ -1,7 +1,26 @@
 package ie.atu.labexam;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> display (MethodArgumentNotValidException ex)
+    {
+        Map<String, String> errorList = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors())
+        {
+            String errorName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errorList.put(errorName, errorMessage);
+        }
+        return ResponseEntity.status(400).body(errorList);
+    }
 }
--- src/main/java/ie/atu/labexam/Employee.java
+++ src/main/java/ie/atu/labexam/Employee.java
@@ -1,17 +1,26 @@
 package ie.atu.labexam;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
+    @Pattern(regexp = "EMP-XXXX", message = "Employee Code must be unique!!")
     private String employeeCode;
+    @Length(max = 100, message = "Maximum 100 characters!!!")
     private String name;
+    @Email(message = "Email must be in valid format!!")
     private String email;
+    @Pattern.List(, message = "Error")
+
     private String position;
+    @Positive (message = "Salary must be greater than 0!!!")
     private float salary;
+    //@PastOrPresent(message = "Must be today or earlier!!!")
     private String dateOfJoining;
 }
